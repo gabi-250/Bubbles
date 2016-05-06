4d8aaf302cbdf785e64ba90650ddcdb348992c43
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-05-06 21:45:01 +0100

    Added a method that checks if the game is over.

--- app/src/main/java/com/example/gabi/bubbles/BubblesModel/Filler.java
+++ app/src/main/java/com/example/gabi/bubbles/BubblesModel/Filler.java
@@ -19,23 +19,56 @@ public class Filler {
     private static final int[] moveColumn = {0, 1, 0, -1};
 
     /**
+     * Returns true if there are no more possible moves.
+     * @return true, if the game is over, false otherwise.
+     */
+    public boolean checkGameOver() {
+
+        filledBoard = new boolean[model.getBoard().length][model.getBoard()[0].length];
+
+        for (int i = 0; i < model.getBoard().length; ++i) {
+
+            for (int j = 0; j < model.getBoard()[0].length; ++j) {
+
+                squares = new ArrayList<>();
+
+                fill(i, j, model.getColor(i, j));
+
+                if (squares.size() > 1) {
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * Returns a list of the squares that form an area (of the same color) around the tile
      * with the specified coordinate. The 'fill' algorithm begins at row 'row' and column 'column'
      * @param row the row of the tile
      * @param column the column of the tile
      * @return the list of squares to be replaced.
      */
-    public static List< Pair<Integer, Integer > > getChangedSquares(int row, int column) {
+    public List< Pair<Integer, Integer > > getChangedSquares(int row, int column) {
 
-        filledBoard = new boolean[ model.getBoard().length][ model.getBoard()[0].length ];
-        squares = new ArrayList <> ();
+        filledBoard = new boolean[model.getBoard().length][model.getBoard()[0].length];
+        squares = new ArrayList<>();
 
         fill(row, column, model.getColor(row, column));
 
         return squares;
     }
 
-    private static void fill(int row, int column, int color) {
+    /**
+     * 'Fills' the maximum possible area of tiles of the same color: creates a list with
+     * the tiles forming this area. Starts 'filling' at a position specified as
+     * row, column, color tuples
+     * @param row the current row
+     * @param column the current column
+     * @param color the color of the starting tile
+     */
+    private void fill(int row, int column, int color) {
 
         if( isValid(row, column) && filledBoard[row][column] == false
                 && model.getColor(row, column) == color) {
@@ -49,7 +82,7 @@ public class Filler {
             }
         }
     }
-
+    
     /**
      * Sets the model of this 'filler' to the specified one
      * @param model the model to associate with this 'filler'
@@ -65,7 +98,7 @@ public class Filler {
      * @param column the column of the tile
      * @return true if the row and column are within the bounds of the array, false otherwise
      */
-    private static boolean isValid(int row, int column ) {
+    private boolean isValid(int row, int column ) {
 
         return (row >= 0 && row < filledBoard.length && column >= 0 && column < filledBoard[0].length);
     }
